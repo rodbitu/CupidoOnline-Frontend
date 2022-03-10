7759010cdf62e63415e96ca9f60d52ca96dffb85
Rodrigo Bitu de Moraes <66644281+rodbitu@users.noreply.github.com>
2022-03-10 10:35:50 -0300

    Fix - email

--- src/components/Gmail_API/src/config/smtp.js
+++ src/components/Gmail_API/src/config/smtp.js
@@ -4,6 +4,6 @@
 module.exports = {
     host: "smtp.gmail.com",
     port: 587,
-    user: 'rodrigo.bitu0@gmail.com', // Botar seu email
-    pass: '020402Ro' // Botar a senha do seu gmail
+    user: 'exemplo@gmail.com', // Botar seu email
+    pass: '********' // Botar a senha do seu gmail
 };
